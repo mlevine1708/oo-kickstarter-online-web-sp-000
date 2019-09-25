957bdc9ccb1c2d8d246f7f4657127d15f569e7b2
Micah Levine <micah.levine@outlook.com>
2019-09-25 23:51:52 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -5,10 +5,14 @@ attr_reader :title, :backers
 
 @@all = []
 
-def initialize
+def initialize(title)
   @title = title
   @backers = []
 end
 
+def back_project(project)
+  @backed_projects << project
+  project.add_backer(self) unless project.backers.include?(self)
+end
 
 end
