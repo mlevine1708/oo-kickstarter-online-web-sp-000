c917083bdb695b58cabe55611c67c804896f4484
Micah Levine <micah.levine@outlook.com>
2019-09-25 23:47:46 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,20 @@
+class Backer
+
+  attr_accessor  :title
+
+attr_reader :backed_projects, :name
+
+@@all = []
+
+def initialize(backer)
+  @backer = backer
+  @backed_projects = []
+  @name = name
+end
+
+
+
+
+
+
+end
---
+++ lib/project.rb
@@ -0,0 +1,13 @@
+class Project
+
+attr_reader :title
+
+
+@@all = []
+
+def initialize
+  @title = title
+end
+
+
+end
