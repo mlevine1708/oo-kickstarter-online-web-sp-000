4429001d65837a757b1c222a665354527daf4275
Micah Levine <micah.levine@outlook.com>
2019-09-25 23:53:36 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -12,6 +12,11 @@ def initialize(backer)
   @name = name
 end
 
+def back_project(project)
+  @backed_projects << project
+  project.add_backer(self) unless project.backers.include?(self)
+end
+
 
 
 
