39a509eb99d1d36105476fb8a093e01e718fe4d3
Micah Levine <micah.levine@outlook.com>
2019-09-26 00:01:35 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,17 @@
+class Backer
+
+  attr_accessor  :backed_projects, :name
+
+def initialize(name)
+  @backed_projects = []
+  @name = name
+end
+
+def back_project(project)
+  @backed_projects << project
+  project.backers << self
+end
+
+
+
+end
